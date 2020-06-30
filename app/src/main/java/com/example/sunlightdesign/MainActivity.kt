23d6e2976d36454bfe0b99b797becfa8c0102b89
nursultanrzabekov <nursbrat991229@gmail.com>
2020-07-01 01:11:05 +0600

    design part 2

--- app/src/main/java/com/example/sunlightdesign/MainActivity.kt
+++ app/src/main/java/com/example/sunlightdesign/MainActivity.kt
@@ -7,6 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.glavnaia_avtorizovannyi)
+        setContentView(R.layout.registration_partner)
     }
 }
