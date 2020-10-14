a10f421b92e8a4e85a921eaaf8cedecfb01b7ff5
foodsnow <dut.kht@gmail.com>
2020-10-15 00:53:34 +0600

    minor changes register fifth

--- app/src/main/java/com/example/sunlightdesign/ui/screens/profile/register/RegisterFragmentStepFive.kt
+++ app/src/main/java/com/example/sunlightdesign/ui/screens/profile/register/RegisterFragmentStepFive.kt
@@ -7,11 +7,10 @@ import android.view.ViewGroup
 import com.example.sunlightdesign.R
 import com.example.sunlightdesign.ui.base.StrongFragment
 import com.example.sunlightdesign.ui.screens.profile.ProfileViewModel
+import kotlinx.android.synthetic.main.fragment_register_partner_step_five.*
 
 
 class RegisterFragmentStepFive : StrongFragment<ProfileViewModel>(ProfileViewModel::class) {
-
-//     val viewModel: AuthViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,13 +24,25 @@ class RegisterFragmentStepFive : StrongFragment<ProfileViewModel>(ProfileViewMod
         super.onActivityCreated(savedInstanceViewState)
 
         setListeners()
-
-//        viewModel.getUseCase()
     }
 
-
     private fun setListeners() {
+        registration_partner_step_five_payment_rg.setOnCheckedChangeListener { group, checkedId ->
+            hidePayments()
+            when(checkedId) {
+                paymentByTillRbtn.id -> {}
+                paymentByPayboxRbtn.id -> {}
+                paymentByBvRbtn.id -> {showPaymentByBv()}
+            }
+        }
+    }
 
+    private fun hidePayments() {
+        payByPayboxLayout.visibility = View.GONE
+    }
+
+    private fun showPaymentByBv() {
+        payByPayboxLayout.visibility = View.VISIBLE
     }
 
 }
