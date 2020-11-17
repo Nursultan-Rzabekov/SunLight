a80538c5d6f0c19964853af12a77920e041b54a5
foodsnow <dut.kht@gmail.com>
2020-11-18 01:25:00 +0600

    bug fixes referral link

--- app/src/main/java/com/example/sunlightdesign/ui/screens/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/sunlightdesign/ui/screens/profile/ProfileFragment.kt
@@ -1,15 +1,18 @@
 package com.example.sunlightdesign.ui.screens.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
+import com.example.sunlightdesign.BuildConfig
 import com.example.sunlightdesign.R
 import com.example.sunlightdesign.data.source.dataSource.remote.profile.entity.UserInfo
 import com.example.sunlightdesign.ui.base.StrongFragment
@@ -18,18 +21,25 @@ import com.example.sunlightdesign.ui.screens.profile.edit.EditProfileFragment.Co
 import com.example.sunlightdesign.ui.screens.profile.register.RegisterFragmentStepOne.Companion.USER_ID
 import com.example.sunlightdesign.utils.Constants.Companion.ACTIVITY_ACTIVE
 import com.example.sunlightdesign.utils.DateUtils
+import com.example.sunlightdesign.utils.saveToClipboard
 import com.example.sunlightdesign.utils.toShortenedUserInfo
 import kotlinx.android.synthetic.main.account_base_profile_cardview.*
 import kotlinx.android.synthetic.main.account_invited_all.*
 import kotlinx.android.synthetic.main.account_mini_profile.*
+import kotlinx.android.synthetic.main.account_referral_link.*
 import kotlinx.android.synthetic.main.account_registration_referral.*
 import kotlinx.android.synthetic.main.account_wallet.*
 import kotlinx.android.synthetic.main.fragment_account.*
+import java.nio.file.Paths
 
 class ProfileFragment : StrongFragment<ProfileViewModel>(ProfileViewModel::class) {
 
     private lateinit var invitedAdapter: InvitedAdapter
-    private var userInfo: UserInfo? = null
+    private var referralLink: String? = null
+
+    companion object {
+        private const val REFERRAL_TAG = "referral"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -77,6 +87,14 @@ class ProfileFragment : StrongFragment<ProfileViewModel>(ProfileViewModel::class
             )
             findNavController().navigate(R.id.action_profileFragment_to_editProfileActivity, bundle)
         }
+
+        copyReferralLinkBtn.setOnClickListener {
+            referralLink?.let { saveReferralLinkToClipboard(it) }
+        }
+
+        shareReferralLinkBtn.setOnClickListener {
+            referralLink?.let { shareReferralLink(it) }
+        }
     }
 
     private fun initRecycler() {
@@ -108,7 +126,24 @@ class ProfileFragment : StrongFragment<ProfileViewModel>(ProfileViewModel::class
             getString(R.string.amount_bv, info.user?.wallet?.left_branch_total)
         rightBranchTotalTextView.text =
             getString(R.string.amount_bv, info.user?.wallet?.right_branch_total)
+        referralLink = "${BuildConfig.BASE_URL}${info.user?.referral_link}"
 
         invitedAdapter.setItems(info.children ?: listOf())
+    }
+
+    private fun saveReferralLinkToClipboard(link: String) {
+        saveToClipboard(requireContext(), link, REFERRAL_TAG)
+        Toast.makeText(requireContext(), "Copied", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun shareReferralLink(link: String) {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, link)
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
     }
 }
