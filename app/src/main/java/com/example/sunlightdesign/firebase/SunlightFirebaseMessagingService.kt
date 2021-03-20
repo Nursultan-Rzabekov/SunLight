d67aff45ba250039802002af36548f91fede04de
foodsnow <dut.kht@gmail.com>
2021-03-20 17:20:35 +0600

    notification from firebase done

--- app/src/main/java/com/example/sunlightdesign/firebase/SunlightFirebaseMessagingService.kt
+++ app/src/main/java/com/example/sunlightdesign/firebase/SunlightFirebaseMessagingService.kt
@@ -1,10 +1,18 @@
 package com.example.sunlightdesign.firebase
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.graphics.BitmapFactory
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import com.example.sunlightdesign.R
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import timber.log.Timber
 
-class SunlightFirebaseMessagingService: FirebaseMessagingService() {
+class SunlightFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
@@ -14,5 +22,34 @@ class SunlightFirebaseMessagingService: FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
         Timber.d(remoteMessage.notification?.body)
+        createChannel()
+        val largeIcon = BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher)
+        val notification = NotificationCompat.Builder(
+            this,
+            getString(R.string.default_notification_channel_id)
+        ).setContentTitle(remoteMessage.notification?.title)
+            .setContentText(remoteMessage.notification?.body)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setLargeIcon(largeIcon)
+            .setColor(ContextCompat.getColor(this, R.color.colorPrimary))
+            .setAutoCancel(true)
+            .build()
+
+        val manager = NotificationManagerCompat.from(applicationContext)
+        manager.notify(0, notification)
+    }
+
+    private fun createChannel() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return
+        val channelId = getString(R.string.default_notification_channel_id)
+        val channel = NotificationChannel(
+            channelId,
+            "Sunlight",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        channel.enableLights(true)
+        channel.enableVibration(true)
+        val manager = NotificationManagerCompat.from(applicationContext)
+        manager.createNotificationChannel(channel)
     }
 }
